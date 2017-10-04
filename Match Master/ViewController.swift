29c11a7d97be72ce588611c399ccfa9541705670
Lin Li <linli@Lins-MacBook-Pro.local>
2017-10-04 07:35:56 -0400

    Initial Commit

---
+++ Match Master/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Match Master
+//
+//  Created by Lin Li on 10/4/17.
+//  Copyright © 2017 Lin Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
