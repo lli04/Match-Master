82e380e5e9a3c0645ce618d96e26c73238a17d23
LiamKirkland <liam.b.kirkland@gmail.com>
2017-10-16 08:38:01 -0400

    started coding

---
+++ Match Master/easyGame.swift
@@ -0,0 +1,50 @@
+//
+//  easyGame.swift
+//  Match Master
+//
+//  Created by  on 10/16/17.
+//  Copyright © 2017 Lin Li. All rights reserved.
+//
+
+import UIKit
+
+class easyGame: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    
+    func assignButtons(){
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
