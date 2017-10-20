6692832cbbde01f9df19f347d260e43577f515c2
Lin Li <linli@Lins-MacBook-Pro.local>
2017-10-20 08:11:38 -0400

    kk

--- Match Master/expertGame.swift
+++ Match Master/expertGame.swift
@@ -1,10 +1,4 @@
-//
-//  expertGame.swift
-//  Match Master
-//
-//  Created by Lin Li on 10/19/17.
-//  Copyright © 2017 Lin Li. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,24 +6,24 @@ class expertGame: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createButton()
         // Do any additional setup after loading the view.
     }
 
+    var goButton = UIButton()
+    
+    func createButton() {
+//        goButton.setTitle("", for: .normal)
+//        goButton.setTitleColor(UIColor.clear, for: .normal)
+        goButton.setBackgroundImage(#imageLiteral(resourceName: "Blue"), for: .normal)
+        goButton.frame = CGRect(x: view.frame.width/2, y: view.frame.height/2, width: 20, height: 15)
+        
+        view.addSubview(goButton)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
