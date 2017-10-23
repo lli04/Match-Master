3a07fe8f0019985caacde9d6f01b051d44ede4a5
Lin Li <linli@Lins-MacBook-Pro.local>
2017-10-23 07:59:34 -0400

    buttons

--- Match Master/expertGame.swift
+++ Match Master/expertGame.swift
@@ -4,19 +4,41 @@ import UIKit
 
 class expertGame: UIViewController {
 
+    var buttonArray:[UIButton] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        let btn = UIButton(type: .custom) as UIButton
-        btn.backgroundColor = .blue
-        btn.setTitle("Button", for: .normal)
-        btn.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
-        btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
-        self.view.addSubview(btn)
+//        let btn = UIButton(type: .custom) as UIButton
+//        btn.backgroundColor = .blue
+//        btn.setTitle(nil, for: .normal)
+//        btn.frame = CGRect(x: 100, y: 100, width: 65, height: 50)
+//        btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+//        self.view.addSubview(btn)
+        createButtons()
     }
     @objc func clickMe(sender:UIButton!) {
             print("Button Clicked")
         }
     
+    func createButtons() {
+        for i in 0..<6 {
+            let btn = UIButton(type: .custom) as UIButton
+        btn.backgroundColor = .blue
+        btn.setTitle(nil, for: .normal)
+        btn.frame = CGRect(x: 61 * i + 5, y: 100, width: 65, height: 50)
+        btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+        self.view.addSubview(btn)
+            buttonArray.append(btn)
+        }
+        for i in 0..<6 {
+            let btn = UIButton(type: .custom) as UIButton
+            btn.backgroundColor = .blue
+            btn.setTitle(nil, for: .normal)
+            btn.frame = CGRect(x: 61 * i + 5, y: 100, width: 65, height: 50)
+            btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+            self.view.addSubview(btn)
+            buttonArray.append(btn)
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
