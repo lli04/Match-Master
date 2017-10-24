add0771700fea38bbf511aba9782ceba72485119
antman425 <aviguy@yahoo.com>
2017-10-24 08:24:31 -0400

    fixed medium buttons

--- Match Master/mediumGame.swift
+++ Match Master/mediumGame.swift
@@ -1,35 +1,61 @@
-//
-//  mediumGame.swift
-//  Match Master
-//
-//  Created by  on 10/19/17.
-//  Copyright © 2017 Lin Li. All rights reserved.
-//
+
 
 import UIKit
 
 class mediumGame: UIViewController {
-
+    
+    var buttonArray:[UIButton] = []
+    
+    var screenWidth: CGFloat = 0.0
+    var screenHeight: CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        screenWidth = UIScreen.main.bounds.width
+        screenHeight = UIScreen.main.bounds.height
+        //        let btn = UIButton(type: .custom) as UIButton
+        //        btn.backgroundColor = .blue
+        //        btn.setTitle(nil, for: .normal)
+        //        btn.frame = CGRect(x: 100, y: 100, width: 65, height: 50)
+        //        btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+        //        self.view.addSubview(btn)
+        createButtons()
     }
-
+    @objc func clickMe(sender:UIButton!) {
+        print("Button Clicked")
+    }
+    
+    func createButtons() {
+        let c = Int((screenWidth - 100.0) / 4.0)
+        let h = Int((screenHeight - 300.0) / 5.0)
+        
+        for i in 0..<4 {
+            for j in 0..<5 {
+                let btn = UIButton(type: .custom) as UIButton
+                btn.backgroundColor = .blue
+                btn.setTitle(nil, for: .normal)
+                btn.frame = CGRect(x: (c + 20) * i + 25, y: (h + 20) * j + 69, width: c, height: h)
+                btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+                self.view.addSubview(btn)
+                buttonArray.append(btn)
+            }
+            
+        }
+        //        for i in 0..<8 {
+        //            let btn = UIButton(type: .custom) as UIButton
+        //            btn.backgroundColor = .blue
+        //            btn.setTitle(nil, for: .normal)
+        //            btn.frame = CGRect(x: 61 * i + 5, y: 100, width: 65, height: 50)
+        //            btn.addTarget(self, action: #selector(clickMe), for: .touchUpInside)
+        //            self.view.addSubview(btn)
+        //            buttonArray.append(btn)
+        //        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
